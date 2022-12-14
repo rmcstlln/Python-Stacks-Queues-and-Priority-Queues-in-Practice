e3d3e4490a425c15cbf540594379984684fe13af
rmcstlln <romarkpinero68@gmail.com>
2022-12-14 13:47:05 +0800

    Created Queue Class

---
+++ queues.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+
+class Queue:
+    def __init__(self):
+        self._elements = deque()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
