135871d26bacd74a5bed2dfd9ccae87a2ad3d28c
rmcstlln <romarkpinero68@gmail.com>
2022-12-14 16:32:22 +0800

    Dataclass

--- PriorityQueueExample.py
+++ PriorityQueueExample.py
@@ -1,14 +1,24 @@
 from queues import PriorityQueue
 
+@dataclass
+class Message:
+    event: str
+
+
 CRITICAL = 3
 IMPORTANT = 2
 NEUTRAL = 1
 
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
+
+wipers < hazard_lights
+
 messages = PriorityQueue()
-messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(IMPORTANT, wipers)
 messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
-messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
-messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
 
 messages.dequeue()
 print(messages)
