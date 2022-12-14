ac44f1f26b21f766df284a1807cd820fb9ef2bfb
rmcstlln <romarkpinero68@gmail.com>
2022-12-14 15:08:42 +0800

    Heappop Example

---
+++ PriorityQueueHeappop.py
@@ -0,0 +1,10 @@
+from heapq import heappush, heappop
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+print(fruits)
+
+heappop(fruits)
+print(fruits)
