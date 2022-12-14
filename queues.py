f97f3b26b19091f14315a7282e0624ff4c605ea2
rmcstlln <romarkpinero68@gmail.com>
2022-12-14 16:44:40 +0800

    Mixin Class

--- queues.py
+++ queues.py
@@ -3,7 +3,16 @@ from heapq import heappop, heappush
 from itertools import count
 
 
-class Queue:
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+
+class Queue(IterableMixin):
     def __init__(self, *elements):
         self._elements = deque(elements)
 
@@ -26,7 +35,7 @@ class Stack(Queue):
         return self._elements.pop()
 
 
-class PriorityQueue:
+class PriorityQueue(IterableMixin):
     def __init__(self):
         self._elements = []
         self._counter = count()
