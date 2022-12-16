d68bc3e88ca4a0ddee96eaa386ea769797d64f6e
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 01:15:15 +0800

    Seventh Graph Code Run

---
+++ graphCode7.py
@@ -0,0 +1,17 @@
+from graph import (
+    City,
+    load_graph,
+    breadth_first_traverse,
+    breadth_first_search as bfs,
+)
+
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city = bfs(graph, nodes["edinburgh"], is_twentieth_century)
+city.name
+
+
+for city in breadth_first_traverse(graph, nodes["edinburgh"]):
+    print(city.name)
