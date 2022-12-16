548f8c2840f14d52cc8424f7884a7f23d3f7c0bf
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 07:12:57 +0800

    Producer Class

--- ThreadSafe.py
+++ ThreadSafe.py
@@ -1,9 +1,8 @@
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
 import threading
-from random import randint
+from random import choice, randint
 from time import sleep
-
 
 
 QUEUE_TYPES = {
@@ -80,3 +79,16 @@ class Worker(threading.Thread):
             for _ in range(100):
                 sleep(delay / 100)
                 self.progress += 1
+
+
+class Producer(Worker):
+    def __init__(self, speed, buffer, products):
+        super().__init__(speed, buffer)
+        self.products = products
+
+    def run(self):
+        while True:
+            self.product = choice(self.products)
+            self.simulate_work()
+            self.buffer.put(self.product)
+            self.simulate_idle()
