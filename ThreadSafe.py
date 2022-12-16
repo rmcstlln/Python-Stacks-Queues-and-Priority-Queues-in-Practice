823ef930ffb55c489c3de64d9b8fa3f085c66ccb
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 07:09:11 +0800

    Worker is Currentrly Idle

--- ThreadSafe.py
+++ ThreadSafe.py
@@ -1,6 +1,9 @@
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
 import threading
+from random import randint
+from time import sleep
+
 
 
 QUEUE_TYPES = {
@@ -48,6 +51,7 @@ PRODUCTS = (
     ":yo-yo:",
 )
 
+
 class Worker(threading.Thread):
     def __init__(self, speed, buffer):
         super().__init__(daemon=True)
@@ -56,3 +60,23 @@ class Worker(threading.Thread):
         self.product = None
         self.working = False
         self.progress = 0
+
+        @property
+        def state(self):
+            if self.working:
+                return f"{self.product} ({self.progress}%)"
+            return ":zzz: Idle"
+
+        def simulate_idle(self):
+            self.product = None
+            self.working = False
+            self.progress = 0
+            sleep(randint(1, 3))
+
+        def simulate_work(self):
+            self.working = True
+            self.progress = 0
+            delay = randint(1, 1 + 15 // self.speed)
+            for _ in range(100):
+                sleep(delay / 100)
+                self.progress += 1
