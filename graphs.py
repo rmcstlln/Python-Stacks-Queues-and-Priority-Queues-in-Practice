3a8eafb79f217339abda14f5b7a60fb9273aec42
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 02:02:37 +0800

    Depth-First Traversal

--- graphs.py
+++ graphs.py
@@ -2,6 +2,7 @@ from typing import NamedTuple
 import networkx as nx
 from queues import Queue
 from collections import deque
+from queues import Queue, Stack
 
 class City(NamedTuple):
     name: str
@@ -78,3 +79,16 @@ def retrace(previous, source, destination):
 
 def connected(graph, source, destination):
     return shortest_path(graph, source, destination) is not None
+
+def depth_first_traverse(graph, source, order_by=None):
+    stack = Stack(source)
+    visited = set()
+    while stack:
+        if (node := stack.dequeue()) not in visited:
+            yield node
+            visited.add(node)
+            neighbors = list(graph.neighbors(node))
+            if order_by:
+                neighbors.sort(key=order_by)
+            for neighbor in reversed(neighbors):
+                stack.enqueue(neighbor)
