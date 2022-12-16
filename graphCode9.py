f953cc072d2c534be5e6aa3dc94e7296727a7239
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 02:09:00 +0800

    Ninth Graph Code Run

---
+++ graphCode9.py
@@ -0,0 +1,17 @@
+from graph import (
+    City,
+    load_graph,
+    depth_first_traverse,
+    depth_first_search as dfs,
+)
+
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+city = dfs(graph, nodes["edinburgh"], is_twentieth_century)
+city.name
+
+
+for city in depth_first_traverse(graph, nodes["edinburgh"]):
+    print(city.name)
