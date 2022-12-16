b0a525db36b355a28daa6e943ed932472184c053
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 02:11:52 +0800

    Connected Altogether

--- graphs.py
+++ graphs.py
@@ -3,7 +3,8 @@ import networkx as nx
 from queues import Queue
 from collections import deque
 from queues import Queue, Stack
-
+from math import inf as infinity
+from queues import MutableMinHeap, Queue, Stack
 class City(NamedTuple):
     name: str
     country: str
@@ -107,3 +108,24 @@ def recursive_depth_first_traverse(graph, source, order_by=None):
                 yield from visit(neighbor)
 
     return visit(source)
+
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
+    while unvisited:
+        visited.add(node := unvisited.dequeue())
+        for neighbor, weights in graph[node].items():
+            if neighbor not in visited:
+                weight = weight_factory(weights)
+                new_distance = unvisited[node] + weight
+                if new_distance < unvisited[neighbor]:
+                    unvisited[neighbor] = new_distance
+                    previous[neighbor] = node
+
+    return retrace(previous, source, destination)
