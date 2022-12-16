7b87500b79d1994460751ab81681caa0cac5724e
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 01:08:44 +0800

    Seventh Code Run

--- graphs.py
+++ graphs.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple
 import networkx as nx
+from queues import Queue
 
 class City(NamedTuple):
     name: str
@@ -28,3 +29,18 @@ def load_graph(filename, node_factory):
         (nodes[name1], nodes[name2], weights)
         for name1, name2, weights in graph.edges(data=True)
     )
+
+def breadth_first_traverse(graph, source):
+    queue = Queue(source)
+    visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
+        for neighbor in graph.neighbors(node):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+
+def breadth_first_search(graph, source, predicate):
+    for node in breadth_first_traverse(graph, source):
+        if predicate(node):
+            return node
