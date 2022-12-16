1f651db5d118e4f65f53284a91b860fcd6e15f45
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 07:26:03 +0800

    Adding Missing Pieces to Fetch HTML links

---
+++ async_queues.py
@@ -0,0 +1,46 @@
+import argparse
+import asyncio
+from collections import Counter
+import aiohttp
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+
+async def main(args):
+    session = aiohttp.ClientSession()
+    try:
+        links = Counter()
+        display(links)
+    finally:
+        await session.close()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-d", "--max-depth", type=int, default=2)
+    parser.add_argument("-w", "--num-workers", type=int, default=3)
+    return parser.parse_args()
+
+
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main(parse_args()))
+
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
+
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features="html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
