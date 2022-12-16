1a52762462cf90e2e599286a85297698ccad7949
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 00:48:13 +0800

    Fourth Graph Code Run

---
+++ graphCode4.py
@@ -0,0 +1,8 @@
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
