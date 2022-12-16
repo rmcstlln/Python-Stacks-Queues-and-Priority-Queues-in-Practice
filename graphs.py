bae166dad0e08e557e7d4ba68af98fcbf8b3f41a
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 01:28:00 +0800

    Created Another Function

--- graphs.py
+++ graphs.py
@@ -44,3 +44,20 @@ def breadth_first_search(graph, source, predicate):
     for node in breadth_first_traverse(graph, source):
         if predicate(node):
             return node
+
+def shortest_path(graph, source, destination, order_by=None):
+    queue = Queue(source)
+    visited = {source}
+    previous = {}
+    while queue:
+        node = queue.dequeue()
+        neighbors = list(graph.neighbors(node))
+        if order_by:
+            neighbors.sort(key=order_by)
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+                previous[neighbor] = node
+                if neighbor == destination:
+                    return retrace(previous, source, destination)
