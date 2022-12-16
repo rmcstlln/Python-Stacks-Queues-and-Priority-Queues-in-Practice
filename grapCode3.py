953c357c1ecde2174a8fd0f5d90ae3260bca051a
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 00:43:32 +0800

    Third Graph Code Run

---
+++ grapCode3.py
@@ -0,0 +1,5 @@
+from graph import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
