8b6f90c8cc2557452474a140bd755af4580fa429
rmcstlln <romarkpinero68@gmail.com>
2022-12-17 01:17:59 +0800

    Eighth Graph Code Run

---
+++ graphCode8.py
@@ -0,0 +1,10 @@
+import networkx as nx
+from graph import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", " → ".join(city.name for city in path))
